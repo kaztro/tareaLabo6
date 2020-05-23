10217d4e2fe96aa40ccc27c45d3b5ef57b62613e
kaztro <00229017@uca.edu.sv>
2020-05-23 16:43:44 -0600

    Init commit

---
+++ src/main/java/com/uca/capas/TareaLabo6XApplication.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaLabo6XApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaLabo6XApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/uca/capas/dao/ContribuyenteDAO.java
@@ -0,0 +1,5 @@
+package com.uca.capas.dao;
+
+public interface ContribuyenteDAO {
+
+}
