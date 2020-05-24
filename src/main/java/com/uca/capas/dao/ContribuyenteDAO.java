6f2d9050fb983b67b145c4a567401ca565dc6a8f
kaztro <00229017@uca.edu.sv>
2020-05-23 18:18:35 -0600

    Sigo empezando :U

--- src/main/java/com/uca/capas/dao/ContribuyenteDAO.java
+++ src/main/java/com/uca/capas/dao/ContribuyenteDAO.java
@@ -1,5 +1,12 @@
 package com.uca.capas.dao;
 
-public interface ContribuyenteDAO {
+import java.util.List;
 
+import org.springframework.dao.DataAccessException;
+
+import com.uca.capas.domain.Contribuyente;
+
+public interface ContribuyenteDAO {
+	public List<Contribuyente> findAll() throws DataAccessException;
+	public void insert(Contribuyente contribuyente) throws DataAccessException;
 }
